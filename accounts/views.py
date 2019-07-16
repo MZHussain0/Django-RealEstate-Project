ca4bcd245e03e29c37dda65e29407a0eea63bfd3
Zakir Hussain <mzhussaino92@gmail.com>
2019-07-17 00:22:39 +0530

    Dashboard and dynamic page titles is setup

--- accounts/views.py
+++ accounts/views.py
@@ -47,14 +47,27 @@ def register(request):
 def login(request):
 
     if request.method == 'POST':
-        # register user
-        pass
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You sucessfully logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')
 
 
 def dashboard(request):
